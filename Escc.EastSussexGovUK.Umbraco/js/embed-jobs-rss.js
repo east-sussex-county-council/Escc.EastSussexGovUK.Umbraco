43aa7ee1526cea85f73652e3c0efecd3187945b6
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-01-11 10:23:04 +0000

    Finialise embedding jobs RSS feeds into pages

--- Escc.EastSussexGovUK.Umbraco/js/embed-jobs-rss.js
+++ Escc.EastSussexGovUK.Umbraco/js/embed-jobs-rss.js
@@ -2,8 +2,43 @@
     $(function() {
         "use strict";
 
-        var feedLinks = $(".embed.rss a");
-        $.get(feedLinks[0].href + "?altTemplate=JobsRssAsTable", function (html) {
+        function addParameterToQueryString(url, parameters) {
+            // Based on https://samaxes.com/2011/09/change-url-parameters-with-jquery/
+
+            /*
+             * queryParameters -> handles the query string parameters
+             * queryString -> the query string without the first '?' character
+             * re -> the regular expression
+             * m -> holds the string matching the regular expression
+             */
+            var queryPos = url.indexOf("?"), queryParameters = {}, re = /([^&=]+)=([^&]*)/g, m;
+            var queryString = (queryPos > -1) ? url.substring(queryPos + 1) : "";
+            url = (queryPos > -1) ? url.substring(0, queryPos) : url;
+
+            // Creates a map with the query string parameters
+            while (m = re.exec(queryString)) {
+                queryParameters[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+            }
+
+            // Add new parameters or update existing ones
+            for (var i = 0; i < parameters.length; i++) {
+                queryParameters[parameters[i].key] = parameters[i].value;
+            };
+
+            /*
+             * Replace the query portion of the URL.
+             * jQuery.param() -> create a serialized representation of an array or
+             *     object, suitable for use in a URL query string or Ajax request.
+             */
+            return url + "?" + $.param(queryParameters);
+        }
+
+        // Look for embedded links to RSS feeds, load the linked URL using a template for embedding and replace it with the resulting HTML
+        var feedLinks = $(".rss.embed-jobs-rss a");
+        var templateParams = [{ key: 'altTemplate', value: 'JobsRssAsTable' }];
+        feedLinks[0].href = addParameterToQueryString(feedLinks[0].href, templateParams);
+
+        $.get(feedLinks[0].href, function (html) {
             feedLinks.parent().replaceWith(html);
         });
     });
