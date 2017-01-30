7094d96528cdd40125565082b000e1a17502adfb
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-01-30 12:59:53 +0000

    Add event tracking to the 'apply' button for use with Google Analytics goal tracking

---
+++ Escc.EastSussexGovUK.Umbraco/js/job-advert.js
@@ -0,0 +1,4 @@
+﻿if (typeof(jQuery) !== 'undefined' && typeof (ga) !== 'undefined') {
+    // Add event tracking on the 'Apply' button
+    $(".main-action a").click(function () { ga('send', 'event', 'job', "apply", $("#job-reference").html(), 0); });
+}
