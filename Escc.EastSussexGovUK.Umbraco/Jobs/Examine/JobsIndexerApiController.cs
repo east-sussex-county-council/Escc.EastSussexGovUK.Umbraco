e7baf8c53702c975f206ed24110037b05eedb631
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-03-16 14:58:43 +0000

    Add an API call which checks whether the jobs lookup values index needs to be updated, and updates it, to fix a situation where a restart caused both indexes to be rebuilt in parallel when they need to be rebuilt in sequence.

--- Escc.EastSussexGovUK.Umbraco/Jobs/Examine/JobsIndexerApiController.cs
+++ Escc.EastSussexGovUK.Umbraco/Jobs/Examine/JobsIndexerApiController.cs
@@ -7,6 +7,7 @@ using Examine.LuceneEngine.Providers;
 using Exceptionless;
 using Umbraco.Core.Logging;
 using Umbraco.Web.WebApi;
+using System.Threading.Tasks;
 
 namespace Escc.EastSussexGovUK.Umbraco.Jobs.Examine
 {
@@ -31,6 +32,43 @@ namespace Escc.EastSussexGovUK.Umbraco.Jobs.Examine
                 LogHelper.Error<JobsIndexerApiController>("Failed to reindex jobs. There may be another thread currently writing to the index.", ex);
                 return Request.CreateResponse(HttpStatusCode.InternalServerError);
             }
+        }
+
+        [HttpGet]
+        public HttpResponseMessage UpdateJobsLookupValues()
+        {
+            try
+            {
+                // If the lookups index is built before the jobs index then it will be be full of 0 values.
+                // This happens often when Azure moves the site to a new machine and the indexes are rebuilt in parallel,
+                // so provide a way to check for missing values and rebuild the lookups index
+                UpdateIndexIfNoLookups("PublicJobsLookupValues");
+                UpdateIndexIfNoLookups("RedeploymentJobsLookupValues");
+
+                return Request.CreateResponse(HttpStatusCode.NoContent);
+            }
+            catch (Exception ex)
+            {
+                ex.ToExceptionless().Submit();
+                LogHelper.Error<JobsIndexerApiController>("Failed to reindex jobs. There may be another thread currently writing to the index.", ex);
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+        private static void UpdateIndexIfNoLookups(string indexPrefix)
+        {
+            var dataSource = new JobsLookupValuesFromExamine(ExamineManager.Instance.SearchProviderCollection[indexPrefix + "Searcher"]);
+            var lookupValues = Task.Run(async () => await dataSource.ReadJobTypes());
+            var jobFound = false;
+            foreach (var lookupValue in lookupValues.Result)
+            {
+                if (lookupValue.Count > 0)
+                {
+                    jobFound = true;
+                    break;
+                }
+            }
+            if (!jobFound) UpdateIndex(indexPrefix + "Indexer");
         }
 
         private static void UpdateIndex(string indexerName)
