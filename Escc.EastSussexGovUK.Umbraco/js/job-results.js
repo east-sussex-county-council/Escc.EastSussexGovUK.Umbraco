5c81d5f7f78b36355e8635f6f3f5f8f68f9deb47
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-04-13 17:11:49 +0100

    It was possible to submit an empty job alerts form, which leads to an error. Use JavaScript and HTML5 validation and sensible defaults to make that harder to do.

---
+++ Escc.EastSussexGovUK.Umbraco/js/job-results.js
@@ -0,0 +1,36 @@
+﻿if (typeof(jQuery) !== 'undefined') {
+    $(function() {
+        "use strict";
+
+        // Validate the email alerts form to make it harder to send bad data to the job alerts service
+        $("form.job-alerts").submit(function () {
+            var errors = [];
+            if (!$("input[name=vDeliveryFrequency]:checked").length) {
+                errors.push("Please select how often you want to get alerts");
+            }
+            if (!$("input[name=dExpirationDate]:checked").length) {
+                errors.push("Please select how long you want to get alerts");
+            }
+            if (!$("#semail").val()) {
+                errors.push("Please enter the address you would like alerts to be sent to");
+            }
+            var validEmail = new RegExp("^[0-9A-Za-z'\.\-_]{1,127}@[0-9A-Za-z'\.\-_]{1,127}$");
+            if (!validEmail.test($("#semail").val())) {
+                errors.push("Please enter a valid email address to send the alerts to");
+            }
+
+            if (errors.length) {
+                var errorList = $("<ul>");
+                for (var i = 0; i < errors.length; i++) {
+                    errorList.append($("<li>").text(errors[i]));
+                }
+                $(".job-alert-errors").remove();
+                $('<div class="errorSummary job-alert-errors">').append(errorList).insertBefore("form.job-alerts");
+                return false;
+            }
+
+            return true;
+        });
+
+    });
+}
