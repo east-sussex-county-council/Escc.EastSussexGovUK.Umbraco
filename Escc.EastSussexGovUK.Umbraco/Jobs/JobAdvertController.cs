319e02bbab27370f09744ca3687dae457008ce22
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-07-19 16:43:21 +0100

    Links to expired jobs from TalentLink job alerts should show our 'job gone' view

--- Escc.EastSussexGovUK.Umbraco/Jobs/JobAdvertController.cs
+++ Escc.EastSussexGovUK.Umbraco/Jobs/JobAdvertController.cs
@@ -1,27 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Web;
-using System.Web.Caching;
 using System.Web.Mvc;
 using Escc.EastSussexGovUK.Umbraco.Examine;
 using Escc.EastSussexGovUK.Umbraco.Jobs.Examine;
-using Escc.EastSussexGovUK.Umbraco.Jobs.TalentLink;
-using Escc.EastSussexGovUK.Umbraco.Models;
 using Escc.EastSussexGovUK.Umbraco.Services;
-using Escc.Net;
 using Escc.Umbraco.Caching;
 using Escc.Umbraco.ContentExperiments;
 using Escc.Umbraco.PropertyTypes;
 using Examine;
-using Exceptionless.Extensions;
-using Umbraco.Core.Models;
 using Umbraco.Web;
 using Umbraco.Web.Models;
 using Umbraco.Web.Mvc;
-using X.PagedList;
 using Task = System.Threading.Tasks.Task;
 using Escc.EastSussexGovUK.Umbraco.Errors;
 using Escc.Dates;
@@ -65,28 +56,37 @@ namespace Escc.EastSussexGovUK.Umbraco.Jobs
                     return new RedirectResult(viewModel.Job.Url.ToString(), true);
                 }
             }
-
-            // Show page for preferred URL
-            var jobUrlSegment = Regex.Match(Request.Url.AbsolutePath, "/([0-9]+)/");
-            if (jobUrlSegment.Success)
-            {
-                var jobsProvider = new JobsDataFromExamine(ExamineManager.Instance.SearchProviderCollection[viewModel.ExamineSearcher], new QueryBuilder(new LuceneTokenisedQueryBuilder(), new KeywordsTokeniser(), new LuceneStopWordsRemover(), new WildcardSuffixFilter()), new RelativeJobUrlGenerator(new Uri(model.Content.UrlAbsolute())));
-                viewModel.Job = Task.Run(async () => await jobsProvider.ReadJob(jobUrlSegment.Groups[1].Value)).Result;
-                if (viewModel.Job.Id == 0 || viewModel.Job.ClosingDate < DateTime.Today)
-                {
-                    // The job URL looks valid but the job isn't in the index, so it's probably closed.
-                    // Find some similar jobs to suggest the user may want to apply for instead.
-                    FindSimilarJobs(jobsProvider, viewModel);
-                }
-            }
             else
             {
-                // The page was accessed by its default Umbraco URL with no parameters. Returning HttpNotFoundResult() ends up with a generic browser 404, 
-                // so to get our custom one we need to look it up and transfer control to it.
-                var notFoundUrl = new HttpStatusFromConfiguration().GetCustomUrlForStatusCode(404);
-                if (notFoundUrl != null && Server != null)
+                // Show page for preferred URL
+                var jobUrlSegment = Regex.Match(Request.Url.AbsolutePath, "/([0-9]+)/");
+                if (jobUrlSegment.Success)
                 {
-                    Server.TransferRequest(notFoundUrl + "?404;" + HttpUtility.UrlEncode(Request.Url.ToString()));
+                    var jobsProvider = new JobsDataFromExamine(ExamineManager.Instance.SearchProviderCollection[viewModel.ExamineSearcher], new QueryBuilder(new LuceneTokenisedQueryBuilder(), new KeywordsTokeniser(), new LuceneStopWordsRemover(), new WildcardSuffixFilter()), new RelativeJobUrlGenerator(new Uri(model.Content.UrlAbsolute())));
+                    viewModel.Job = Task.Run(async () => await jobsProvider.ReadJob(jobUrlSegment.Groups[1].Value)).Result;
+                    if (viewModel.Job.Id == 0 || viewModel.Job.ClosingDate < DateTime.Today)
+                    {
+                        // The job URL looks valid but the job isn't in the index, so it's probably closed.
+                        // Find some similar jobs to suggest the user may want to apply for instead.
+                        FindSimilarJobs(jobsProvider, viewModel);
+                    }
+                }
+                else
+                {
+                    // The page was accessed by its default Umbraco URL with no parameters. Returning HttpNotFoundResult() ends up with a generic browser 404, 
+                    // so to get our custom one we need to look it up and transfer control to it.
+                    
+                    // However, we need to check for "alttemplate" because a link from a TalentLink job alert or the TalentLink back office to an expired job
+                    // is a link to the default Umbraco URL with just a querystring that doesn't match any data. That in turn will want to load the jobs CSS for
+                    // a job that matches nothing, and that request ends up here. We want it to continue and return the JobsCss view, not our 404 page.
+                    if (String.IsNullOrEmpty(Request.QueryString["alttemplate"]))
+                    {
+                        var notFoundUrl = new HttpStatusFromConfiguration().GetCustomUrlForStatusCode(404);
+                        if (notFoundUrl != null && Server != null)
+                        {
+                            Server.TransferRequest(notFoundUrl + "?404;" + HttpUtility.UrlEncode(Request.Url.ToString()));
+                        }
+                    }
                 }
             }
 
