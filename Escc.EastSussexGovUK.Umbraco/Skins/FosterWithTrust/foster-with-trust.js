ee908ccbb2b9574fd79702156bcbb48ebf912868
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-11-09 14:27:06 +0000

    Update 'Foster with trust' skin with new design

--- Escc.EastSussexGovUK.Umbraco/Skins/FosterWithTrust/foster-with-trust.js
+++ Escc.EastSussexGovUK.Umbraco/Skins/FosterWithTrust/foster-with-trust.js
@@ -1,6 +1,9 @@
 ﻿if (typeof (jQuery) !== 'undefined') {
     $(function () {
+        // Load two copies of the logo for different resolutions. The <picture> element or srcset attribute could handle this better, but the design wouldn't work in IE11.
         var domain = (document.location.hostname === "localhost") ? "" : "https://www.eastsussex.gov.uk";
-        $('<div class="content-small content-medium supporting foster-with-trust-logo"><img src="' + domain + '/skins/fosterwithtrust/foster-with-trust.png" alt="Foster with trust" /></div>').insertBefore($(".article").first());
+        $('<img src="' + domain + '/skins/fosterwithtrust/foster-with-trust-small.png" alt="Foster with trust" class="foster-with-trust-logo small content" />' +
+          '<img src="' + domain + '/skins/fosterwithtrust/foster-with-trust.png" alt="Foster with trust" class="foster-with-trust-logo medium large content" />' +
+          '<p class="foster-with-trust-call"><span class="aural">Call us: </span>01323 464129</p>').insertAfter($("#1_smallCrumb"));
     });
 }
