ce1e608aadb052efccfd6a8d11d6483ddc6ed3dd
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-11-09 14:50:04 +0000

    Rename image to ensure the old version is not loaded from cache

--- Escc.EastSussexGovUK.Umbraco/Skins/FosterWithTrust/foster-with-trust.js
+++ Escc.EastSussexGovUK.Umbraco/Skins/FosterWithTrust/foster-with-trust.js
@@ -3,7 +3,7 @@
         // Load two copies of the logo for different resolutions. The <picture> element or srcset attribute could handle this better, but the design wouldn't work in IE11.
         var domain = (document.location.hostname === "localhost") ? "" : "https://www.eastsussex.gov.uk";
         $('<img src="' + domain + '/skins/fosterwithtrust/foster-with-trust-small.png" alt="Foster with trust" class="foster-with-trust-logo small content" />' +
-          '<img src="' + domain + '/skins/fosterwithtrust/foster-with-trust.png" alt="Foster with trust" class="foster-with-trust-logo medium large content" />' +
+          '<img src="' + domain + '/skins/fosterwithtrust/foster-with-trust-medium.png" alt="Foster with trust" class="foster-with-trust-logo medium large content" />' +
           '<p class="foster-with-trust-call"><span class="aural">Call us: </span>01323 464129</p>').insertAfter($("#1_smallCrumb"));
     });
 }
