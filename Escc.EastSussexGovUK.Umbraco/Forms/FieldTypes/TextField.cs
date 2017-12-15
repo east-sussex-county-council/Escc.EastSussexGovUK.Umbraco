63d9380c9ac1e3ee61a6d0a0c6e5a434855cde48
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-12-15 15:09:31 +0000

    The personal data text field can extend the original type rather than presenting a confusing alternative

---
+++ Escc.EastSussexGovUK.Umbraco/Forms/FieldTypes/TextField.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Forms.Core;
+using Umbraco.Forms.Core.Providers.FieldTypes;
+using Umbraco.Forms.Core.Attributes;
+
+namespace Escc.EastSussexGovUK.Umbraco.Forms.FieldTypes
+{
+    /// <summary>
+    /// A text field for Umbraco Forms with an additional property for displaying a pop-up privacy notice
+    /// </summary>
+    /// <seealso cref="Umbraco.Forms.Core.FieldType" />
+    public class TextField : Textfield
+    {
+        [Setting("Privacy notice", description = "What happens to the data?")]
+        public string PrivacyNotice { get; set; }
+    }
+}
