2ee3421c4056979d38c215c84f428d6e4bcbf103
Rick Mason <richard.mason@eastsussex.gov.uk>
2017-12-19 17:24:37 +0000

    Add a way to view Umbraco Forms entries which separates having access to the form from having the Manage Forms permission required to design a form

---
+++ Escc.EastSussexGovUK.Umbraco/App_Plugins/UmbracoFormsEntries/UmbracoFormsEntriesTreeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using UmbracoForms = Umbraco.Forms;
+using Umbraco.Core;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.Trees;
+using Umbraco.Forms.Data.Storage;
+
+namespace Escc.EastSussexGovUK.Umbraco.App_Plugins.Escc.UmbracoFormsAdmin
+{
+    [Tree("forms", "formEntries", "Form entries")]
+    [PluginController("UmbracoFormsEntries")]
+    public class UmbracoFormsEntriesTreeController : TreeController
+    {
+        protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
+        {
+            // check if we're rendering the root node's children
+            if (id == Constants.System.Root.ToInvariantString())
+            {
+                var tree = new TreeNodeCollection();
+                
+                 using (FormStorage formStorage = new FormStorage())
+                {
+                    IEnumerable<UmbracoForms.Core.Form> forms = formStorage.GetForms();
+                    foreach (UmbracoForms.Core.Form form in forms)
+                    {
+                        if (UmbracoForms.Web.FormsSecurity.HasAccessToForm(form.Id))
+                        {
+                            tree.Add(CreateTreeNode(form.Id.ToString(), id, queryStrings, form.Name, "icon-autofill"));
+                        }
+                    }
+                }
+                return tree;
+            }
+            // this tree doesn't support rendering more than 1 level
+            throw new NotSupportedException();
+        }
+        protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
+        {
+            return null;
+        }
+    }
+}
