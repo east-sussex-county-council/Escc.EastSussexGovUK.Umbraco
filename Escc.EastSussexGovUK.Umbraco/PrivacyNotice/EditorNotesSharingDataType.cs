a79f1cfb6305aaed4f02d5df49f6a5e556c06baf
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-06-21 16:52:15 +0100

    Remove text from privacy notices - requested by Data Protection Officer

--- Escc.EastSussexGovUK.Umbraco/PrivacyNotice/EditorNotesSharingDataType.cs
+++ Escc.EastSussexGovUK.Umbraco/PrivacyNotice/EditorNotesSharingDataType.cs
@@ -28,8 +28,6 @@ namespace Escc.EastSussexGovUK.Umbraco.PrivacyNotice
                     "the sharing of your personal data to such persons.</p>" +
                 "<p>We may use the information we hold about you to assist in the detection and prevention of crime or fraud. " +
                     "We may also share this information with other bodies that inspect and manage public funds.</p>" +
-                "<p>We will not routinely disclose any information about you without your express permission. However, there are " +
-                    "circumstances where we must or can share information about you owing to a legal or statutory obligation.</p>" +
                    "<h4><strong>Use of third party organisations</strong></h4>" +
                    "<p>East Sussex County Council may share your information with trusted external organisations to process your data on our behalf.</p>",1)},
                {"hideLabel",new PreValue(-1,"1",2)},
