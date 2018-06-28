92f55a749a004b88f715eeafcdd271baf90d31a5
Rick Mason <richard.mason@eastsussex.gov.uk>
2018-06-28 15:22:48 +0100

    Change the format for embedding an iCaseWork disclosure log

--- Escc.EastSussexGovUK.Umbraco/js/icasework-forms.js
+++ Escc.EastSussexGovUK.Umbraco/js/icasework-forms.js
@@ -2,7 +2,7 @@ if (typeof (jQuery) !== 'undefined' && typeof (iFrameResize) !== 'undefined') {
     "use strict";
 
     $(function () {
-        var iCaseWork = new RegExp(/^(https?:\/\/[A-Za-z0-9_-]+\.icasework\.com\/(form\?[A-Za-z0-9=&_-]+|servlet\/ep.appSearch;.*))$/);
+        var iCaseWork = new RegExp(/^(https?:\/\/[A-Za-z0-9_-]+\.icasework\.com\/(form|cases)\?[A-Za-z0-9=&_%-]+)$/);
         $("a.embed").filter(function (index) {
             return iCaseWork.test(this.href);
         }).each(function () {
