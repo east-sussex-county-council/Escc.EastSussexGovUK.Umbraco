5ccf12c7aced4a1b798d2ffb0115f17dd69d5005
Rick Mason <richard.mason@eastsussex.gov.uk>
2019-04-10 10:34:13 +0100

    The job reference no longer includes a department code, so for searching in analytics add the department to the job reference

--- Escc.EastSussexGovUK.Umbraco.Web/js/job-advert.js
+++ Escc.EastSussexGovUK.Umbraco.Web/js/job-advert.js
@@ -1,10 +1,11 @@
-﻿if (typeof(jQuery) !== 'undefined' && typeof (ga) !== 'undefined') {
-    $("a.apply").click(function () { ga('send', 'event', 'job', "apply", $("#job-reference").html(), 0); });
-    $("a.apply-mobile").click(function () { ga('send', 'event', 'job', "apply (mobile button)", $("#job-reference").html(), 0); });
-    $("a.JD-DocumentName").click(function () { ga('send', 'event', 'job', "download job description", $("#job-reference").html(), 0); });
-    $(".login-continue a").click(function () { ga('send', 'event', 'job', "continue application", $("#job-reference").html(), 0); });
-    $(".share a[href*='friend.aspx']").click(function () { ga('send', 'event', 'job', "share by email", $("#job-reference").html(), 0); });
-    $(".share a[href^='https://www.facebook.com']").click(function () { ga('send', 'event', 'job', "share on Facebook", $("#job-reference").html(), 0); });
-    $(".share a[href^='https://twitter.com']").click(function () { ga('send', 'event', 'job', "share on Twitter", $("#job-reference").html(), 0); });
-    $(".share a[href^='https://www.linkedin.com']").click(function () { ga('send', 'event', 'job', "share on LinkedIn", $("#job-reference").html(), 0); });
+﻿if (typeof (jQuery) !== 'undefined' && typeof (ga) !== 'undefined') {
+    let eventLabel = $("#job-reference").html() + '/' + $("#department").html();
+    $("a.apply").click(function () { ga('send', 'event', 'job', "apply", eventLabel, 0); });
+    $("a.apply-mobile").click(function () { ga('send', 'event', 'job', "apply (mobile button)", eventLabel, 0); });
+    $("a.JD-DocumentName").click(function () { ga('send', 'event', 'job', "download job description", eventLabel, 0); });
+    $(".login-continue a").click(function () { ga('send', 'event', 'job', "continue application", eventLabel, 0); });
+    $(".share a[href*='friend.aspx']").click(function () { ga('send', 'event', 'job', "share by email", eventLabel, 0); });
+    $(".share a[href^='https://www.facebook.com']").click(function () { ga('send', 'event', 'job', "share on Facebook", eventLabel, 0); });
+    $(".share a[href^='https://twitter.com']").click(function () { ga('send', 'event', 'job', "share on Twitter", eventLabel, 0); });
+    $(".share a[href^='https://www.linkedin.com']").click(function () { ga('send', 'event', 'job', "share on LinkedIn", eventLabel, 0); });
 }
