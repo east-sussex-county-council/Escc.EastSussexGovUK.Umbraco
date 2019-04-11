86bc03f18731475d21b6ce552fef97f4031696c8
Rick Mason <richard.mason@eastsussex.gov.uk>
2019-04-11 16:29:09 +0100

    Update the search fields and validation for rights of way modification order applications

--- Escc.EastSussexGovUK.Umbraco.Web/RightsOfWayModifications/RightsOfWayModificationsEventHandler.cs
+++ Escc.EastSussexGovUK.Umbraco.Web/RightsOfWayModifications/RightsOfWayModificationsEventHandler.cs
@@ -14,6 +14,7 @@ using Umbraco.Core.Logging;
 using Exceptionless;
 using Escc.Umbraco.PropertyEditors.PersonNamePropertyEditor;
 using System.Globalization;
+using Umbraco.Core.Events;
 
 namespace Escc.EastSussexGovUK.Umbraco.Web.RightsOfWayModifications
 {
@@ -33,6 +34,8 @@ namespace Escc.EastSussexGovUK.Umbraco.Web.RightsOfWayModifications
             {
                 ExamineManager.Instance.IndexProviderCollection["RightsOfWayModificationsIndexer"].GatheringNodeData += RightsOfWayEventHandler_GatheringNodeData;
             }
+
+            ContentService.Saving += ContentService_Saving;
         }
 
         private void RightsOfWayEventHandler_GatheringNodeData(object sender, IndexingNodeDataEventArgs e)
@@ -56,12 +59,43 @@ namespace Escc.EastSussexGovUK.Umbraco.Web.RightsOfWayModifications
                             combinedFields.AppendLine(e.Fields["pageDescription"]);
                         }
 
-                        var ownerConverter = new PersonNamePropertyValueConverter();
+                        if (e.Fields.ContainsKey("nearestTownOrVillage"))
+                        {
+                            combinedFields.AppendLine(e.Fields["nearestTownOrVillage"]);
+                        }
+
+                        if (e.Fields.ContainsKey("statusClaimed"))
+                        {
+                            combinedFields.AppendLine(e.Fields["statusClaimed"]);
+                        }
+
+                        var nameConverter = new PersonNamePropertyValueConverter();
+                        if (e.Fields.ContainsKey("nameOfApplicant"))
+                        {
+                            var applicant = nameConverter.ConvertDataToSource(null, e.Fields["nameOfApplicant"], false);
+                            if (applicant != null) combinedFields.AppendLine(applicant.ToString());
+                        }
+
+                        if (e.Fields.ContainsKey("nameOfApplicantOrganisation"))
+                        {
+                            combinedFields.AppendLine(e.Fields["nameOfApplicantOrganisation"]);
+                        }
+
+                        if (e.Fields.ContainsKey("applicationStatus"))
+                        {
+                            combinedFields.AppendLine(e.Fields["applicationStatus"]);
+                        }
+
+                        if (e.Fields.ContainsKey("decision"))
+                        {
+                            combinedFields.AppendLine(e.Fields["decision"]);
+                        }
+
                         for (var i = 1; i <= 5; i++)
                         {
                             if (e.Fields.ContainsKey($"Owner{i}"))
                             {
-                                var owner = ownerConverter.ConvertDataToSource(null, e.Fields[$"Owner{i}"], false);
+                                var owner = nameConverter.ConvertDataToSource(null, e.Fields[$"Owner{i}"], false);
                                 if (owner != null) combinedFields.AppendLine(owner.ToString());
                             }
 
@@ -93,8 +127,39 @@ namespace Escc.EastSussexGovUK.Umbraco.Web.RightsOfWayModifications
             }
         }
 
+        /// <summary>
+        /// Name of applicant is required but it can be in either of two fields. Standard required validation can't handle that, so check and return a custom error.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ContentService_Saving(IContentService sender, SaveEventArgs<IContent> e)
+        {
+            var nameConverter = new PersonNamePropertyValueConverter();
+            foreach (IContent contentItem in e.SavedEntities)
+            {
+                try
+                {
+                    if (contentItem.ContentType.Alias == "RightsOfWayModification")
+                    {
+                        var individualApplicant = nameConverter.ConvertDataToSource(null, contentItem.GetValue<string>("nameOfApplicant"), false) as PersonName;
+                        var organisationApplicant = contentItem.GetValue<string>("nameOfApplicantOrganisation");
+                        if (individualApplicant == null && string.IsNullOrEmpty(organisationApplicant))
+                        {
+                            e.CancelOperation(new EventMessage("Invalid", "The applicant's name is required – either an individual or an organisation", EventMessageType.Error));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Error<Exception>($"Error validating the name of applicant fields for node {contentItem.Id}", ex);
+                    ex.ToExceptionless().Submit();
+                }
+            }
+        }
+
         public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
         }
     }
 }
+ 
