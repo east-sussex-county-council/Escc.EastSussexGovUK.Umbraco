71d0f40bfcf0508f6e6af728f260210ae2876567
Rick Mason <richard.mason@eastsussex.gov.uk>
2019-05-20 15:57:36 +0100

    Review requirements for preview mode - unsafe content security policy no longer required

---
+++ Escc.EastSussexGovUK.Umbraco.Web/js/umbraco-preview.js
@@ -0,0 +1,7 @@
+﻿if (typeof(jQuery) != 'undefined') {
+    $(function () {
+        // Because Umbraco preview loads in an iframe over HTTPS, it won't frame an HTTP page because it's insecure (mixed content ).
+        // Rather than having links to HTTP pages lead to a blank iframe, use a frame-busting script to break out of the iframe, which lets authors test that their links work.
+        $("a[href^='http:']").attr("target", "_top");
+    });
+}
